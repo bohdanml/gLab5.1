f7603b46bc86f1fd53471e484ff4e85112dfc832
Bogdan <Bogdan@VITOPC>
2023-11-08 20:38:31 +0200

    Add project files.

---
+++ gLab5.1/lab.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double a, const double b);
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h(s * s, 1 + t) + h(1, s * t)) / 1 + h(s, t) * h(s, t);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b)
+{
+	return (a + b + (a * a) * (b * b)) / ((a * a) + (b * b));
+}
